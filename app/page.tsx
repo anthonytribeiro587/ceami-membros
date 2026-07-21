8745995d1e5237887525737c6d2ad493337199d8
Anthony Ribeiro Web Systems Developer | JS Stack <anthony.tribeiro587@gmail.com>
2026-07-21 00:11:00 -0300

    feat: expose courses module from main dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import MemberApp from './MemberApp';
+import CoursesShortcut from './components/CoursesShortcut';
 
 export default function Page() {
-  return <MemberApp />;
+  return (
+    <>
+      <MemberApp />
+      <CoursesShortcut />
+    </>
+  );
 }

c1e07171a973c8cc0dce824d1cd382e4aa6bff1f
Anthony Ribeiro Web Systems Developer | JS Stack <anthony.tribeiro587@gmail.com>
2026-07-22 14:36:13 -0300

    ui: activate responsive course table

--- app/cursos/page.tsx
+++ app/cursos/page.tsx
@@ -2,6 +2,7 @@ import CoursesWorkspace from './CoursesWorkspace';
 import CoursePortalSession from './CoursePortalSession';
 import CourseDeletionControls from './CourseDeletionControls';
 import QrCodeReliability from './QrCodeReliability';
+import CourseTablePortal from './CourseTablePortal';
 import './courses.css';
 import './course-table.css';
 import './course-deletion.css';
@@ -10,6 +11,7 @@ export default function CoursesPage() {
   return (
     <>
       <CoursesWorkspace />
+      <CourseTablePortal />
       <CoursePortalSession />
       <CourseDeletionControls />
       <QrCodeReliability />

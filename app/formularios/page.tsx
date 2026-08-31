c2aa4ef0643c09c0dc4c4898d6328894ed838cfa
Anthony Ribeiro Web Systems Developer | JS Stack <anthony.tribeiro587@gmail.com>
2026-08-31 11:48:48 -0300

    fix: normalize seminar data when admin opens forms

--- app/formularios/page.tsx
+++ app/formularios/page.tsx
@@ -3,6 +3,7 @@ import { getCurrentUiRole } from '@/lib/server/current-profile';
 import FormulariosClient from './FormulariosClient';
 import InlinePaymentsEnhancement from './InlinePaymentsEnhancement';
 import EditSubmissionEnhancement from './EditSubmissionEnhancement';
+import SeminarDataSync from './SeminarDataSync';
 import './formularios.css';
 
 export default async function FormulariosPage() {
@@ -12,6 +13,7 @@ export default async function FormulariosPage() {
       <FormulariosClient />
       <InlinePaymentsEnhancement />
       <EditSubmissionEnhancement />
+      <SeminarDataSync />
     </AdminRouteShell>
   );
 }

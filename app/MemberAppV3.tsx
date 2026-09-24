b5b46fbedc46f650ab8aaa2c86004aaac1cf7318
Anthony Ribeiro Web Systems Developer | JS Stack <anthony.tribeiro587@gmail.com>
2026-09-24 11:44:42 -0300

    refactor: navigate standalone modules through CEAMI launcher

--- app/MemberAppV3.tsx
+++ app/MemberAppV3.tsx
@@ -12,7 +12,6 @@ import {
   Check,
   ChevronRight,
   Church,
-  GraduationCap,
   LayoutDashboard,
   LogOut,
   Mail,
@@ -375,11 +374,6 @@ export default function MemberAppV3({ initialIsAdmin = false }: { initialIsAdmin
             </Link>
           )}
 
-          {isAdmin && (
-            <Link href="/cursos" prefetch onClick={() => setMenuOpen(false)}>
-              <GraduationCap size={19} /><span>Cursos</span>
-            </Link>
-          )}
 
         </nav>
 
@@ -570,7 +564,7 @@ function Dashboard({ members, onOpen, onRefresh, onNew }: {
           </div>
           <div className="member-v3-quick-actions">
             <button type="button" onClick={onNew}><UserRound /><span>Novo membro</span></button>
-            <button type="button" onClick={() => window.location.assign('/cursos')}><GraduationCap /><span>Ver cursos</span></button>
+            <button type="button" onClick={() => window.location.assign('/?selecionar=1')}><LayoutGrid /><span>Aplicativos</span></button>
           </div>
         </aside>
       </div>
